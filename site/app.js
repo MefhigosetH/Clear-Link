03c116e30090951fd84880a29aeb83cba473e983
Victor Villarreal <mefhigoseth@gmail.com>
2022-02-17 22:47:40 -0300

    (ui): First basic UI

---
+++ site/app.js
@@ -0,0 +1,49 @@
+/*
+* ClearLink app logic.
+* Developed by Victor Villarreal @ Feb 2022
+* https://github.com/MefhigosetH/Clear-Link
+* Source code with No license
+*/
+
+
+
+/*
+* Init the NavBar.
+*
+* @return None
+*/
+function initNavBar() {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    };
+
+    console.log('NavBar initialized');
+}
+
+
+
+/*
+* Init the app when document is loaded.
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Initialize NavBar
+    initNavBar();
+});
